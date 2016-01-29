7d730e2a717370c2fa886aadf23763d0bb7b8745
Adrien Dorsaz <adrien@adorsaz.ch>
2016-01-29 20:07:34 +0100

    erGetContacts: let JS engine to handle arrays

--- components/erGetContacts.js
+++ components/erGetContacts.js
@@ -125,12 +125,7 @@ erGetContactsRequest.prototype = {
 
 		var rm = aResp.XPath("/s:Envelope/s:Body/m:FindItemResponse/m:ResponseMessages/m:FindItemResponseMessage/m:ResponseCode");
 
-		var contacts = [];
-		var tmpList = aResp.XPath("/s:Envelope/s:Body/m:GetItemResponse/m:ResponseMessages/m:GetItemResponseMessage/m:Items/*");
-		for (var index in tmpList) {
-			contacts.push(tmpList[index]);
-		}
-		tmpList = null;
+		var contacts =  aResp.XPath("/s:Envelope/s:Body/m:GetItemResponse/m:ResponseMessages/m:GetItemResponseMessage/m:Items/*").slice();
 
 		if (this.mCbOk) {
 			this.mCbOk(this, contacts);
